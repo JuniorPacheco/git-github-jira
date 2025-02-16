9514cfc826ee95a41b298cff87717daf6fc116b2
Junior Pacheco <junior.pacheco.356@gmail.com>
2025-02-16 14:18:21 -0500

    add funtionality to add products to cart and render it

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,6 +2,12 @@ export const ShoppingCart = () => {
   return (
     <div>
       <h1>ShoppingCart</h1>
+
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
     </div>
   );
 };
